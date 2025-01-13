1981cb6379981dc46aa4b86a46b8457fe6aa93b0
Victor Mwenda techmystic/ <126842172+Queror254@users.noreply.github.com>
2025-01-13 14:13:22 +0300

    -

--- task_management/tasks/urls.py
+++ task_management/tasks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    # the ulrs are implemented in the main app using the rest_framework DefaultRouter,
 ]
